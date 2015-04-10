8a8f12d587a28caf4d09ebed2e0c78547287a7ea
Mario Andrés Correa <a@macool.me>
2015-04-10 12:18:57 -0500

    tweaking for docker

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+web: bundle exec rails s -p 3016
 worker: bundle exec sidekiq -C ./config/sidekiq.yml
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,16 @@
 # This file is overwritten on deploy
 
+url = if ENV['REDIS_1_PORT_6379_TCP_ADDR'].present?
+    address = ENV['REDIS_1_PORT_6379_TCP_ADDR']
+    port = ENV['REDIS_1_PORT_6379_TCP_PORT']
+    "redis://#{address}:#{port}"
+  else
+    "redis://localhost:6379"
+  end
+
 Sidekiq.configure_client do |config|
-  config.redis = { :namespace => 'signon' }
+  config.redis = { url: url, namespace: "signon" }
 end
 Sidekiq.configure_server do |config|
-  config.redis = { :namespace => 'signon' }
+  config.redis = { url: url, namespace: "signon" }
 end
